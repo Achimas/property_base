e2c094be303bb8e888dae85bbf94953259dd5a61
Achimas <agafonovrn.it@gmail.com>
2024-10-25 18:32:46 +0700

    Main app is ready

--- property_base/main/views.py
+++ property_base/main/views.py
@@ -4,13 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) -> HttpResponse:
     context = {
-        'title': 'Home',
-        'content': "Main page of Property base",
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': True
+        'title': 'Home - Main',
+        'content': "Danang rental agency"
     }
     return render(request=request, template_name='main/index.html', context=context)
 
 def about(request) -> HttpResponse:
-    return HttpResponse('About page')
+    context = {
+    'title': 'Home - About',
+    'content': "About us",
+    'text_on_page': "Welcome to our Da Nang real estate website! We are a team of professionals dedicated to helping you find the perfect property—whether residential or commercial—in one of the most scenic and rapidly growing cities in Vietnam: Da Nang. Our mission is to make the process of buying, selling, or renting property smooth, transparent, and stress-free for every client."
+    }
+    return render(request=request, template_name='main/about.html', context=context)
